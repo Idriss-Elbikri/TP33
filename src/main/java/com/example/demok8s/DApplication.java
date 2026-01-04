47ae2244c009f8e773289e6eac04fec09ded86e8
Idriss-Elbikri <elbikriidriss@gmail.com>
2026-01-04 18:47:46 +0100

    TP33

--- src/main/java/com/example/demok8s/DApplication.java
+++ src/main/java/com/example/demok8s/DApplication.java
@@ -1,10 +1,8 @@
 package com.example.demok8s;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
-public class DemoK8sApplication {
+public class DApplication {
     public static void main(String[] args) {
         SpringApplication.run(DemoK8sApplication.class, args);
     }
--- src/main/java/com/example/demok8s/api/HController.java
+++ src/main/java/com/example/demok8s/api/HController.java
@@ -1,11 +1,8 @@
 package com.example.demok8s.api;
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import java.util.Map;
-
 @RestController
 public class HelloController {
 
